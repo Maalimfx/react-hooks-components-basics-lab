8b820e09ca4f97abc5cb16dafdea41bec5b71902
abass <abass@DESKTOP-478KRS4.>
2025-04-15 08:28:21 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -13,20 +13,31 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
     </div>
+  ); 
+}
+
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
   );
 }
 
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
+
+
+
 
 export default App;
